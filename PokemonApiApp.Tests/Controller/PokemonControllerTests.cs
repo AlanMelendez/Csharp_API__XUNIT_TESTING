b91ed5a699cc53904dbde3a909f77ceaf15c1a82
Alan Cuevas Melendez <73464077+AlanMelendez@users.noreply.github.com>
2025-03-07 02:17:00 -0600

    add: Create first test to GetPokemons endpoint; create arrange,act and assert

--- PokemonApiApp.Tests/Controller/PokemonControllerTests.cs
+++ PokemonApiApp.Tests/Controller/PokemonControllerTests.cs
@@ -5,7 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Controllers;
+using PokemonReviewApp.Dto;
 using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
 
 namespace PokemonApiApp.Tests.Controller
 {
@@ -21,5 +26,24 @@ namespace PokemonApiApp.Tests.Controller
             _reviewRepository = A.Fake<IReviewRepository>();
             _mapper = A.Fake<IMapper>();
         }
+
+        [Fact]
+        public void GetPokemons_ReturnOk()
+        {
+            //Arrange
+            var pokemons = A.Fake<ICollection<PokemonDto>>();
+            var pokemonsList = A.Fake<List<PokemonDto>>();
+
+            A.CallTo(() => _mapper.Map<List<PokemonDto>>(pokemons)).Returns(pokemonsList);
+
+            var controller = new PokemonController(_pokemonRepository, _reviewRepository, _mapper);
+
+            //Act
+            var result = controller.GetPokemons();
+
+            //Assert
+            result.Should().NotBeNull();
+        }
+
     }
 }
