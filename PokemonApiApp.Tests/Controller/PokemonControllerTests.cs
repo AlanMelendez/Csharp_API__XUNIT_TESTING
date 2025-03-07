f7de92a948765035981eb25a24b7766b8b09fec3
Alan Cuevas Melendez <73464077+AlanMelendez@users.noreply.github.com>
2025-03-07 01:33:13 -0600

    add: Install FakeItEasy and FluentAssertions; Create mocks reference to PokemonController

---
+++ PokemonApiApp.Tests/Controller/PokemonControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using FakeItEasy;
+using PokemonReviewApp.Interfaces;
+
+namespace PokemonApiApp.Tests.Controller
+{
+    public class PokemonControllerTests 
+    {
+        private readonly IPokemonRepository _pokemonRepository;
+        private readonly IReviewRepository _reviewRepository;
+        private readonly IMapper _mapper;
+
+        public PokemonControllerTests()
+        {
+            _pokemonRepository = A.Fake<IPokemonRepository>();
+            _reviewRepository = A.Fake<IReviewRepository>();
+            _mapper = A.Fake<IMapper>();
+        }
+    }
+}
---
+++ PokemonApiApp.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace PokemonApiApp.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
